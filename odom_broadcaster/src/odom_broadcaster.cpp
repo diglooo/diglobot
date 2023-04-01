ff06a6dc7f336195e81b2856e0c4f3e544e8ae93
diglo <davidedigloria87@gmail.com>
2023-04-01 11:30:26 +0200

    update

--- odom_broadcaster/src/odom_broadcaster.cpp
+++ odom_broadcaster/src/odom_broadcaster.cpp
@@ -1,43 +1,32 @@
 #include "ros/ros.h"
-#include "std_msgs/Int16.h"
+//#include "std_msgs/Int16.h"
 #include "std_msgs/Float32.h"
 #include <nav_msgs/Odometry.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2_ros/transform_broadcaster.h>
+//#include <geometry_msgs/PoseStamped.h>
+//#include <tf2/LinearMath/Quaternion.h>
+//#include <tf2_ros/transform_broadcaster.h>
+#include <tf/transform_broadcaster.h>
 #include <cmath>
  
-// Create odometry data publishers
-ros::Publisher odom_data_pub;
-ros::Publisher odom_data_pub_quat;
-nav_msgs::Odometry odomNew;
-nav_msgs::Odometry odomOld;
- 
+
 // Initial pose
-const double initialX = 0.0;
-const double initialY = 0.0;
-const double initialTheta = 0.00000000001;
+double orientSum=0, posXSum=0, posYSum=0;
 const double PI = 3.141592;
  
 // Robot physical constants
-const double WHEEL_BASE = 0.37; // Center of left tire to center of right tire
+const double WHEEL_BASE = 0.38; // Center of left tire to center of right tire
  
 // Distance both wheels have traveled
 double distanceLeft = 0;
 double distanceRight = 0;
  
- 
-using namespace std;
- 
-
-
 ros::Time l_lasttime, l_currtime;
 void Calc_Left(const std_msgs::Float32& leftSpeed) 
 {
 	l_currtime=ros::Time::now();
 	ros::Duration dt=l_currtime-l_lasttime;
 	l_lasttime=l_currtime;
-	distanceLeft += -leftSpeed.data * dt.toSec();
+	distanceLeft += leftSpeed.data * dt.toSec();
 }
  
 ros::Time r_lasttime, r_currtime;
@@ -48,147 +37,94 @@ void Calc_Right(const std_msgs::Float32& rightSpeed)
 	r_lasttime=r_currtime;
 	distanceRight += rightSpeed.data * dt.toSec();
 }
- 
-// Publish a nav_msgs::Odometry message in quaternion format
-void publish_quat() {
- 
-  tf2::Quaternion q;
-         
-  q.setRPY(0, 0, odomNew.pose.pose.orientation.z);
- 
-  nav_msgs::Odometry quatOdom;
-  quatOdom.header.stamp = odomNew.header.stamp;
-  quatOdom.header.frame_id = "map";
-  quatOdom.child_frame_id = "base_link_fake";
-  quatOdom.pose.pose.position.x = odomNew.pose.pose.position.x;
-  quatOdom.pose.pose.position.y = odomNew.pose.pose.position.y;
-  quatOdom.pose.pose.position.z = odomNew.pose.pose.position.z;
-  quatOdom.pose.pose.orientation.x = q.x();
-  quatOdom.pose.pose.orientation.y = q.y();
-  quatOdom.pose.pose.orientation.z = q.z();
-  quatOdom.pose.pose.orientation.w = q.w();
-  quatOdom.twist.twist.linear.x = odomNew.twist.twist.linear.x;
-  quatOdom.twist.twist.linear.y = odomNew.twist.twist.linear.y;
-  quatOdom.twist.twist.linear.z = odomNew.twist.twist.linear.z;
-  quatOdom.twist.twist.angular.x = odomNew.twist.twist.angular.x;
-  quatOdom.twist.twist.angular.y = odomNew.twist.twist.angular.y;
-  quatOdom.twist.twist.angular.z = odomNew.twist.twist.angular.z;
- 
-  for(int i = 0; i<36; i++) {
-    if(i == 0 || i == 7 || i == 14) {
-      quatOdom.pose.covariance[i] = .01;
-     }
-     else if (i == 21 || i == 28 || i== 35) {
-       quatOdom.pose.covariance[i] += 0.1;
-     }
-     else {
-       quatOdom.pose.covariance[i] = 0;
-     }
-  }
- 
-  odom_data_pub_quat.publish(quatOdom);
-}
- 
-// Update odometry information
-double lastDist=0, lastDiffDist=0;
-void update_odom() 
+  
+int main(int argc, char **argv) 
 {
- 
-	// Calculate the average distance
-	double totDist=(distanceRight + distanceLeft) / 2;
-	double cycleDistance = totDist-lastDist;
-	lastDist=totDist;
-   
-	// Calculate the number of radians the robot has turned since the last cycle
-	double totDiffDist= (distanceLeft- distanceRight);
-	double cycleDiffDistance = totDiffDist-lastDiffDist;
-	lastDiffDist=totDiffDist;
+	// Launch ROS and create a node
+	ros::init(argc, argv, "odometry_publisher");
+	ros::NodeHandle node;
+	
+	ros::Publisher odom_pub;
+	tf::TransformBroadcaster odom_broadcaster;	
 
-  double cycleAngle = asin((cycleDiffDistance)/WHEEL_BASE);
- 
- 
-  // Calculate the new pose (x, y, and theta)
-  odomNew.pose.pose.position.x = odomOld.pose.pose.position.x + cos(cycleAngle)*cycleDistance;
-  odomNew.pose.pose.position.y = odomOld.pose.pose.position.y + sin(cycleAngle)*cycleDistance;
-  
-  odomNew.pose.pose.orientation.z = odomOld.pose.pose.orientation.z + cycleAngle;
-  
-       
-  if (odomNew.pose.pose.orientation.z > PI) {
-    odomNew.pose.pose.orientation.z -= 2*PI;
-  }
-  else if (odomNew.pose.pose.orientation.z < -PI) {
-    odomNew.pose.pose.orientation.z += 2*PI;
-  }
- 
-  // Make sure theta stays in the correct range
-  if (odomNew.pose.pose.orientation.z > PI) {
-    odomNew.pose.pose.orientation.z -= 2 * PI;
-  }
-  else if (odomNew.pose.pose.orientation.z < -PI) {
-    odomNew.pose.pose.orientation.z += 2 * PI;
-  }
-  else{}
- 
-  // Compute the velocity
-  odomNew.header.stamp = ros::Time::now();
-  odomNew.twist.twist.linear.x = cycleDistance/(odomNew.header.stamp.toSec() - odomOld.header.stamp.toSec());
-  odomNew.twist.twist.angular.z = cycleAngle/(odomNew.header.stamp.toSec() - odomOld.header.stamp.toSec());
- 
-  // Save the pose data for the next cycle
-  odomOld.pose.pose.position.x = odomNew.pose.pose.position.x;
-  odomOld.pose.pose.position.y = odomNew.pose.pose.position.y;
-  odomOld.pose.pose.orientation.z = odomNew.pose.pose.orientation.z;
-  odomOld.header.stamp = odomNew.header.stamp;
- 
-  // Publish the odometry message
-  odom_data_pub.publish(odomNew);
-}
- 
-int main(int argc, char **argv) {
-   
-  // Set the data fields of the odometry message
-  odomNew.header.frame_id = "map";
-  odomNew.child_frame_id = "base_link_fake";
-  odomNew.pose.pose.position.z = 0;
-  odomNew.pose.pose.orientation.x = 0;
-  odomNew.pose.pose.orientation.y = 0;
-  odomNew.twist.twist.linear.x = 0;
-  odomNew.twist.twist.linear.y = 0;
-  odomNew.twist.twist.linear.z = 0;
-  odomNew.twist.twist.angular.x = 0;
-  odomNew.twist.twist.angular.y = 0;
-  odomNew.twist.twist.angular.z = 0;
-  odomOld.pose.pose.position.x = initialX;
-  odomOld.pose.pose.position.y = initialY;
-  odomOld.pose.pose.orientation.z = initialTheta;
- 
-  // Launch ROS and create a node
-  ros::init(argc, argv, "odom_pub");
-  ros::NodeHandle node;
- 
-  // Subscribe to ROS topics
-  ros::Subscriber subForRightCounts = node.subscribe("right_wheel_speed", 100, Calc_Right, ros::TransportHints().tcpNoDelay());
-  ros::Subscriber subForLeftCounts = node.subscribe("left_wheel_speed", 100, Calc_Left, ros::TransportHints().tcpNoDelay());
-  
-  // Publisher of simple odom message where orientation.z is an euler angle
-  odom_data_pub = node.advertise<nav_msgs::Odometry>("odom_data_euler", 100);
- 
-  // Publisher of full odom message where orientation is quaternion
-  odom_data_pub_quat = node.advertise<nav_msgs::Odometry>("odom_data_quat", 100);
- 
-  ros::Rate loop_rate(30); 
-  std::cout<<"odom_broadcaster!!"<<endl;
-     
-  while(ros::ok()) 
-  {
-		
-    update_odom();
-    publish_quat();
+	// Subscribe to ROS topics
+	ros::Subscriber subForRightCounts = node.subscribe("right_wheel_speed", 100, Calc_Right, ros::TransportHints().tcpNoDelay());
+	ros::Subscriber subForLeftCounts = node.subscribe("left_wheel_speed", 100, Calc_Left, ros::TransportHints().tcpNoDelay());
 
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
- 
-  return 0;
+	odom_pub = node.advertise<nav_msgs::Odometry>("odom", 100);
+
+	double lastDist=0, lastDiffDist=0;
+	ros::Time lastTime, currTime;
+
+	ros::Rate loop_rate(30); 
+	std::cout<<"odom_broadcaster!!"<<std::endl;
+	 
+	while(node.ok()) 
+	{
+		ros::spinOnce();
+
+		currTime=ros::Time::now();
+		ros::Duration dt=currTime-lastTime;
+		float deltat=dt.toSec();
+		lastTime=currTime;
+
+		if(deltat<0.00001) deltat=1/30;
+
+		// Calculate the average distance
+		double totDist=(distanceRight + distanceLeft) / 2;
+		double cycleDistance = totDist-lastDist;
+		lastDist=totDist;
+
+		// Calculate the number of radians the robot has turned since the last cycle
+		double totDiffDist= (distanceRight - distanceLeft);
+		double cycleDiffDistance = totDiffDist-lastDiffDist;
+		lastDiffDist=totDiffDist;
+		double cycleAngle = asin((cycleDiffDistance)/WHEEL_BASE);
+
+		// Calculate the new pose (x, y, and theta)
+		orientSum += cycleAngle;
+		posXSum += cos(orientSum)*cycleDistance;
+		posYSum += sin(orientSum)*cycleDistance; 
+
+		// Compute the velocity
+		double linearSpd = cycleDistance/deltat;
+		double angularSpd = cycleAngle/deltat;
+		   
+		if (orientSum > PI) orientSum -= 2*PI;
+		if (orientSum < -PI) orientSum += 2*PI;
+		 
+		geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(orientSum);
+
+		nav_msgs::Odometry odometry;
+		odometry.header.stamp = ros::Time::now();
+		odometry.header.frame_id = "odom";
+		odometry.child_frame_id = "base_link";
+		odometry.pose.pose.position.x = posXSum;
+		odometry.pose.pose.position.y = posYSum;
+		odometry.pose.pose.position.z = 0;
+		odometry.pose.pose.orientation=odom_quat;
+		odometry.twist.twist.linear.x = linearSpd;
+		odometry.twist.twist.linear.y = 0;
+		odometry.twist.twist.linear.z = 0;
+		odometry.twist.twist.angular.x = 0;
+		odometry.twist.twist.angular.y = 0;
+		odometry.twist.twist.angular.z = angularSpd;
+
+		geometry_msgs::TransformStamped odom_trans;
+		odom_trans.header.stamp = odometry.header.stamp;
+		odom_trans.header.frame_id = "odom";
+		odom_trans.child_frame_id = "base_link";
+		odom_trans.transform.translation.x = posXSum;
+		odom_trans.transform.translation.y = posYSum;
+		odom_trans.transform.translation.z = 0.0;
+		odom_trans.transform.rotation=odom_quat;
+
+		odom_broadcaster.sendTransform(odom_trans);
+		odom_pub.publish(odometry);
+
+	
+		loop_rate.sleep();
+	}
+	std::cout<<"odom_broadcaster DEAD!!"<<std::endl;
+	return 0;
 }
